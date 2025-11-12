75fc031887d5a9172f00623b95cdb530454403be
j3r0nimo <jerobalort@gmail.com>
2025-11-12 20:02:49 -0300

    modificacion del practico
    creo comando para correr los test por separado y borro ejercicios

--- backend/test/run-security-tests.js
+++ backend/test/run-security-tests.js
@@ -13,11 +13,7 @@ const tests = [
   '01-brute-force.test.js',
   '02-command-injection.test.js',
   '03-csrf-protection.test.js',
-  '04-file-inclusion.test.js',
-  '05-file-upload.test.js',
-  '06-insecure-captcha.test.js',
-  '07-sql-injection.test.js',
-  '08-blind-sql-injection.test.js'
+  '07-sql-injection.test.js'
 ];
 
 let currentTest = 0;
