9bd542440f25bb09d04ccec45f12c670aa52262a
j3r0nimo <jerobalort@gmail.com>
2025-11-19 10:02:44 -0300

    Update vulnerabilities.js

--- backend/src/routes/vulnerabilities.js
+++ backend/src/routes/vulnerabilities.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const vulnerabilityController = require('../controllers/vulnerabilityController');
-const { uploadMiddleware, uploadFile } = require('../controllers/uploadController');
+//const { uploadMiddleware, uploadFile } = require('../controllers/uploadController');
 
 // Command Injection
 router.post('/ping', vulnerabilityController.ping);
 
 // CSRF - Transferencia
-router.post('/transfer', vulnerabilityController.transfer);
+//router.post('/transfer', vulnerabilityController.transfer);
 
 // Local File Inclusion
-router.get('/file', vulnerabilityController.readFile);
+//router.get('/file', vulnerabilityController.readFile);
 
 // File Upload
-router.post('/upload', uploadMiddleware, uploadFile);
+//router.post('/upload', uploadMiddleware, uploadFile);
 
 module.exports = router;
